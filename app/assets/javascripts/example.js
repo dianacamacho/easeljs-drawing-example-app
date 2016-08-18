e02f54eef66ef3e3c6738bd6f1e16538e5eb45b4
dianacamacho <dcam1901@yahoo.com>
2016-08-18 02:11:45 -0500

    define a stage inside the init function

--- app/assets/javascripts/example.js
+++ app/assets/javascripts/example.js
@@ -1,3 +1,7 @@
 function init() {
   // code here.
+
+  // the stage will hold all objects displayed on the canvas
+  // pass in the canvas element id
+  var stage = new createjs.Stage("demoCanvas");
 };
