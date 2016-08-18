48706561d370a57f94c39e47da6a88c5ce0b3348
dianacamacho <dcam1901@yahoo.com>
2016-08-18 02:16:58 -0500

    create a new Bitmap using the photo you want to show on the canvas and load it

--- app/assets/javascripts/example.js
+++ app/assets/javascripts/example.js
@@ -4,4 +4,11 @@ function init() {
   // the stage will hold all objects displayed on the canvas
   // pass in the canvas element id
   var stage = new createjs.Stage("demoCanvas");
+
+  // create a Bitmap, which draws the image given (wrigley field map) onto the canvas
+  var wrigley = new createjs.Bitmap("assets/wrigley.png");
+  // this is just preloading the image, so it will be ready to show on the canvas once it's added to the drawing stage
+  wrigley.image.onload = function() {
+    stage.update();
+  }
 };
